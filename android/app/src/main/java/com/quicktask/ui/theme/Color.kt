bd65a824af589fd4a9465e083e785ffeb550731b
gbariman <102682328+gbariman@users.noreply.github.com>
2025-10-27 21:31:38 +0100

    Update: Change to modern financial blue color scheme

--- android/app/src/main/java/com/quicktask/ui/theme/Color.kt
+++ android/app/src/main/java/com/quicktask/ui/theme/Color.kt
@@ -2,11 +2,17 @@ package com.quicktask.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Modern financial blue colors
+val Blue80 = Color(0xFF5C9EFF)
+val BlueGrey80 = Color(0xFFA0C4E8)
+val LightBlue80 = Color(0xFFB3DAF5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF0066CC)
+val BlueGrey40 = Color(0xFF4A90A4)
+val DarkBlue40 = Color(0xFF004080)
+
+// Primary blue scheme
+val PrimaryBlue = Color(0xFF0066CC)
+val SecondaryBlue = Color(0xFF0052A3)
+val AccentBlue = Color(0xFF3399FF)
 
