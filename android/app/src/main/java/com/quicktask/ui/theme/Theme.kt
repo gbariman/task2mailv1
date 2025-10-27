7c0bd753efb820bf25f9fbcdb5ea49f36dfb56e5
gbariman <102682328+gbariman@users.noreply.github.com>
2025-10-27 21:31:43 +0100

    Update: Apply blue color scheme to theme

--- android/app/src/main/java/com/quicktask/ui/theme/Theme.kt
+++ android/app/src/main/java/com/quicktask/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightBlue80,
+    secondary = BlueGrey80,
+    tertiary = Blue80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryBlue,
+    secondary = SecondaryBlue,
+    tertiary = AccentBlue
 )
 
 @Composable
